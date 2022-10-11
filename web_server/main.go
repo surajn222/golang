1eecdff3916e26b02e9598506cd777caa358e973
Suraj Nathani <surajn222@gmail.com>
2022-10-11 15:28:56 +0530

    updated modules

--- web_server/main.go
+++ web_server/main.go
@@ -3,11 +3,19 @@ package main
 import (
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-func main() {
+func HttpFileHandler(response http.ResponseWriter, request *http.Request) {
+	//fmt.Fprintf(w, "Hi from e %s!", r.URL.Path[1:])
+	response.Header().Set("Content-type", "text/html")
+	fmt.Fprintf(response, "<h1>TEST HEADER<h1>")
+
+}
+
+func web_server() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
@@ -17,6 +25,31 @@ func main() {
 		fmt.Fprintf(w, "Hi")
 	})
 
+	http.HandleFunc("/index", HttpFileHandler)
+
 	log.Fatal(http.ListenAndServe(":8081", nil))
+
+}
+
+func get_request() {
+
+	resp, err := http.Get("http://httpbin.org/ip")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//We Read the response body on the line below.
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	//Convert the body to type string
+	sb := string(body)
+	log.Printf(sb)
+}
+
+func main() {
+
+	//web_server()
+	get_request()
 
 }
